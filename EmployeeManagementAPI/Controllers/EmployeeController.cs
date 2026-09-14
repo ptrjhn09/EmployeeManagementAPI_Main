e73857e4d0ea7e35bd9de42d0a7d0deb49b15016
ptrjhn09 <peter.ricarte@ext.essilor.com>
2026-09-14 16:03:23 +0800

    Revert "Add Comment"
    This reverts commit c9c133b5c60a0b57a2ffb654e275018b96c4914d.

--- EmployeeManagementAPI/Controllers/EmployeeController.cs
+++ EmployeeManagementAPI/Controllers/EmployeeController.cs
@@ -15,8 +15,7 @@ namespace EmployeeManagementAPI.Controllers
     {
         private readonly IEmployeeService _service;
         private readonly IMapper _mapper;
-        //asdsad
-        //llllllllllll
+
         public EmployeeController(IEmployeeService service, IMapper mapper)
         {
             _service = service;
